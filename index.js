1cb0747a8cdaacd98c2500e184c1a395200b9abf
KirillSav1n <KirillSav1n@users.noreply.github.com>
2021-12-02 11:35:16 +0000

    Deploying to gh-pages from @ KirillSav1n/-furniture-site@1c93b5c054842cdc42e1f5e5d39ca3f7d2cd119b 🚀

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 "use strict";
-let product = "Кресло";
+/*let product = "Кресло";
 let price = 10000;
 let cart = `${product}, цена ${price} рублей`;
 alert(cart);
+*/
+
+
+
+
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  if (max<=min){
+    alert ('Ошибка');
+  }
+  else{
+  return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+}
+alert(getRandomInt(1, 10));
+//eslint-disable-next-line
+console.log(getRandomInt);
